49474bdd9f4fc92bb23b96dfe76c3bad745bddcf
Andres Rodriguez Guapacha <arguapacha@acm.org>
2015-10-13 17:29:11 +0100

    Add view with simple html response, just to pass the tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
   def test_root_url_resolves_to_home_page(self):
     found = resolve('/')
     self.assertEqual(found.func, home_page)
+
+  def test_home_page_returns_html(self):
+    request = HttpRequest()
+    response = home_page(request)
+    self.assertTrue(response.content.startswith(b'<html>'))
+    self.assertTrue(response.content.endswith(b'</html>'))
+    self.assertIn(b'<title>To-Do Lists</title>', response.content)
